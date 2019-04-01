15225d11f2ec0c47567bd9d69d504bd8c83d7069
Robisys <robisys@users.noreply.github.com>
2019-04-01 14:27:54 +0200

    Update and rename src1 to src/HelloWorld/SayHello.php

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,10 @@
+    <?php 
+    namespace HelloWorld;
+    class SayHello
+    {
+        public static function world()
+        {
+            return 'Hello World, Composer!';
+        }
+    }
+    
